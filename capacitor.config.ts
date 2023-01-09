43e8e5502e780dfc5cf24c473ab1f57c082d6e2c
latif83 <latifm8360@gmail.com>
2023-01-09 20:06:28 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Niilano_Marketplace',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
