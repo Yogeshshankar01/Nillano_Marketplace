5eb9a7fcabd6a529e2d20845b7d4cbeb7e282939
latif83 <latifm8360@gmail.com>
2023-02-04 20:56:05 +0000

    nice interface

--- src/app/pages/product-details/product-details.module.ts
+++ src/app/pages/product-details/product-details.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { ProductDetailsPageRoutingModule } from './product-details-routing.module';
 
 import { ProductDetailsPage } from './product-details.page';
+import { MenuModule } from 'src/app/components/menu/menu.component.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ProductDetailsPageRoutingModule
+    ProductDetailsPageRoutingModule,
+    MenuModule
   ],
   declarations: [ProductDetailsPage]
 })
