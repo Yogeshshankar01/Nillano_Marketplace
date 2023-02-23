96940dbc59502e4b797683a4a9d3cf668f9e2909
latif83 <latifm8360@gmail.com>
2023-02-23 10:22:17 +0000

    .

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -120,6 +120,7 @@ export class HomePage implements OnDestroy,OnInit {
           this.toast = this.toastController.create({
             message: res.message ? res.message : "Sorry, we're unable to retrieve products at the moment. We're working to fix the issue. Please try again later.",
             color: 'danger',
+            duration: 2000,
             position: 'bottom',
             cssClass: 'flex-contianer',
             buttons: [
