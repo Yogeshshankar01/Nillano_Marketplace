096b4903e156478478ebd491110af606a20be8bf
latif83 <latifm8360@gmail.com>
2023-02-28 18:22:46 +0000

    messaging done

--- src/app/pages/messages/messages.page.ts
+++ src/app/pages/messages/messages.page.ts
@@ -8,84 +8,166 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class MessagesPage implements OnInit {
 
-  chats = [
-    {
-      id: 1, name: "Lawson Shoes",
-      sent: [
-        {
-          from: 1,
-          to: 2,
-          content: "Hello"
-        }
-      ],
-      receive: [
-        {
-          from: 2,
-          to: 1,
-          content: "Hi"
-        }
-      ]
-    },
-    { id: 2, name: "ShoeShopper" },
-    { id: 3, name: "MarketMaverick" },
-    { id: 4, name: "Seller456" },
-    { id: 5, name: "TradeMaster" },
-    { id: 6, name: "Shopaholic789" },
-    { id: 7, name: "LawsonLuxe" },
-    { id: 8, name: "StepUpStyle" },
-    { id: 9, name: "Lawson Shoes" },
-    { id: 10, name: "ShoeShopper" },
-    { id: 11, name: "MarketMaverick" },
-    { id: 12, name: "Seller456" },
-    { id: 13, name: "TradeMaster" },
-    { id: 14, name: "Shopaholic789" },
-    { id: 15, name: "LawsonLuxe" },
-    { id: 16, name: "StepUpStyle" }
-  ]
+  chats: any
+
+  back() {
+    this.router.navigate(['messages'])
+  }
+
 
   customCounterFormatter(inputLength: number, maxLength: number) {
     return `${maxLength - inputLength} characters remaining`;
   }
 
   selectedUser = false
-  selectedUserMessages:any
+  selectedUserMessages: any
 
-  constructor(private activeRoute: ActivatedRoute, private router:Router) { }
+  formatTimestamp(isoString: string): string {
+    const now = new Date();
+    const timestamp = new Date(isoString).getTime();
+    const elapsed = now.getTime() - timestamp;
+
+    const minute = 60 * 1000;
+    const hour = 60 * minute;
+    const day = 24 * hour;
+    const week = 7 * day;
+
+    if (elapsed < minute) {
+      return 'Just now';
+    } else if (elapsed < hour) {
+      const minutes = Math.floor(elapsed / minute);
+      return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+    } else if (elapsed < day) {
+      const hours = Math.floor(elapsed / hour);
+      return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+    } else if (elapsed < 2 * day) {
+      return 'Yesterday';
+    } else if (elapsed < week) {
+      const days = Math.floor(elapsed / day);
+      return `${days} day${days > 1 ? 's' : ''} ago`;
+    } else {
+      const date = new Date(timestamp);
+      return date.toLocaleString();
+    }
+  }
+
+  content = ""
+
+  addChat(userId: number) {
+    
+    this.chats.forEach((element: any) => {
+      if(element.id === userId){
+
+        element.messages.push({
+          id: 4,
+          content: this.content,
+          from: 1,
+          to: userId,
+          createdAt: new Date().toISOString()
+        })
+
+        this.content = ""
+
+      }
+    });
+ 
+  }
+
+
+  constructor(private activeRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
 
+    this.chats = [
+      {
+        "id": 2,
+        "username": "Smith_Stores",
+        "first_name": "Jane",
+        "user_profile": {
+          "url": "https://ionicframework.com/docs/img/demos/avatar.svg",
+          "public_id": "sjjd"
+        },
+        "messages": [
+          {
+            "id": 1,
+            "content": "Hello good morning",
+            "from": 1,
+            "to": 2,
+            "createdAt": "2023-02-27T19:10:28.044Z"
+          },
+          {
+            "id": 3,
+            "content": "Am Latif",
+            "from": 1,
+            "to": 2,
+            "createdAt": "2023-02-27T19:10:28.044Z"
+          },
+          {
+            "id": 4,
+            "content": "Good morning, how are you",
+            "from": 2,
+            "to": 1,
+            "createdAt": "2023-02-27T19:10:28.045Z"
+          }
+        ]
+      },
+      {
+        "id": 3,
+        "username": "Bobby1_Shop",
+        "first_name": "Bob",
+        "user_profile": {
+          "url": "https://ionicframework.com/docs/img/demos/avatar.svg",
+          "public_id": "sjjd"
+        },
+        "messages": [
+          {
+            "id": 5,
+            "content": "hey",
+            "from": 3,
+            "to": 1,
+            "createdAt": "2023-02-27T19:10:28.045Z"
+          },
+          {
+            "id": 6,
+            "content": "hi",
+            "from": 1,
+            "to": 3,
+            "createdAt": "2023-02-27T19:10:28.045Z"
+          }
+        ]
+      }
+    ]
+
     this.activeRoute.queryParams.subscribe(params => {
+
+
+      let chatslist = document.getElementById('chatslist') as HTMLElement
+      let chats = document.getElementById('chats') as HTMLElement
 
       if (!params["chat"]) {
         this.selectedUser = false
+        chatslist.classList.remove('d-none')
+        chats.classList.add('d-none')
       }
+
       else {
         this.selectedUser = true
         let selectedUserId = params["chat"]
-        this.selectedUserMessages = this.chats.find(chat=>(chat.id == selectedUserId))
+        this.selectedUserMessages = this.chats.find((chat: { id: any; }) => (chat.id == selectedUserId))
 
-        let chatslist = document.getElementById('chatslist') as HTMLElement
-        let chats = document.getElementById('chats') as HTMLElement
-        
-        if(window.innerWidth <= 767){
+        if (window.innerWidth <= 767) {
 
-        chatslist.classList.add('d-none')
-        chats.classList.remove('d-none')
+          chatslist.classList.add('d-none')
+          chats.classList.remove('d-none')
 
         }
 
-        else{
+        else {
           chatslist.classList.remove('d-none')
           chats.classList.add('d-none')
         }
 
-        // let backButton = document.getElementById('backButton') as HTMLElement
-        // backButton.addEventListener('click',()=>{
-        //   this.router.navigate(['messages'])
-        // })
 
-        // // this.router.createUrlTree()
-        // window.history.pushState()
 
       }
 
